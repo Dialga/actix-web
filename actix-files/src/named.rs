de8e8aed3c7eefb9ae2fbbed5e392a8b00215ce0
Dialga <Dialga@users.noreply.github.com>
2024-01-07 22:39:43 +1300

    Update named.rs

--- actix-files/src/named.rs
+++ actix-files/src/named.rs
@@ -139,8 +139,12 @@ impl NamedFile {
                 _ => DispositionType::Attachment,
             };
 
-            // Replace newlines in filenames which could occur on some filesystems.
-            let filename_s = filename.replace('\n', "%0A");
+            // Replace newlines and other line breaks in filenames which could occur on some filesystems.
+            let filename_s = filename
+                .replace('\n', "%0A")
+                .replace('\x11', "%0B") // \v vertical tab
+                .replace('\x12', "%0C") // \f form feed
+                .replace('\r', "%0D");
             let mut parameters = vec![DispositionParam::Filename(filename_s)];
 
             if !filename.is_ascii() {
